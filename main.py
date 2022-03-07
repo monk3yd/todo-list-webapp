9d9240836e6c45017fd84c8498680cba5747953b
monk3yd <jegajardog@gmail.com>
2022-03-07 15:46:32 -0300

    Read Tasks from db, dynamic index.html table that render tasks

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class User(UserMixin, db.Model):  # Create Users Table Model
     password = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(100))
 
-    tasks = db.relationship("Task", back_populates='task_author')
+    tasks = db.relationship("Task", back_populates='author')
 
 
 class Task(db.Model):  # Create Tasks Table Model
@@ -35,12 +35,12 @@ class Task(db.Model):  # Create Tasks Table Model
     id = db.Column(db.Integer, primary_key=True)
 
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    task_author = db.relationship('User', back_populates='tasks')
+    author = db.relationship('User', back_populates='tasks')
 
-    task_title = db.Column(db.Text, nullable=False)
-    task_description = db.Column(db.String(30))
-    task_priority = db.Column(db.String(30), nullable=False)
-    task_status = db.Column(db.String(30), nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    description = db.Column(db.String(30))
+    priority = db.Column(db.String(30), nullable=False)
+    status = db.Column(db.String(30), nullable=False)
 
 
 # Create tables in db
@@ -48,9 +48,17 @@ db.create_all()
 
 
 # TODO - Routes
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    # POST
+        # TODO - Receive Add Task btn pressed
+        # TODO - Open Popup Window with form to add to table/db
+        # return
+
+    # GET
+    # Read all from task table in db
+    all_tasks = db.session.query(Task).all()
+    return render_template("index.html", tasks=all_tasks)
 
 
 if __name__ == "__main__":
